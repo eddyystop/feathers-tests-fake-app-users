a2331dfdfb4c3e1981c852bf16fa5649d3e768ad
John Szwaronek <johnsz9999@gmail.com>
2016-07-26 08:53:45 -0400

    Make tests work, update README with code examples

--- README.md
+++ README.md
@@ -1,23 +1,66 @@
 ## feathers-tests-fake-app-users
-Fake some feathers dependencies in service unit tests. Starter for your customized fakes
-
-Work in progress.
-Extracting from production code.
+Fake some Feathers dependencies in service unit tests. Starter for your own customized fakes
 
 [![Build Status](https://travis-ci.org/eddyystop/feathers-tests-fake-app-users.svg?branch=master)](https://travis-ci.org/eddyystop/feathers-tests-fake-app-users)
 [![Coverage Status](https://coveralls.io/repos/github/eddyystop/feathers-tests-fake-app-users/badge.svg?branch=master)](https://coveralls.io/github/eddyystop/feathers-tests-fake-app-users?branch=master)
 
 ## Code Example
 
-To do.
+The following example is from `test/example.js`.
+It may be run with `npm run test:es6`.
+
+In this example we want to perform some unit tests on feathers-service-verify-reset's resend method,
+so we need to fake its Feathers dependencies app and users.
+We would be running integration tests if we used Feathers' actual app and users,
+and tsuch tests would be more complicated.
 
 ```javascript
-const feathers-tests-fake-app-users = require('feathers-tests-fake-app-users');
+const feathersFakes = require('feathers-tests-fake-app-user');
+const verifyResetService = require('feathers-service-verify-reset').service;
+const testUsersDb = [
+  { _id: 'a', email: 'a', isVerified: false, verifyToken: '000', verifyExpires: Date.now() + 5000 },
+];
+
+describe('verifyReset::resend', () => {
+  var db, app, users, verifyReset;
+
+  beforeEach(() => {
+    db = clone(testUsersDb);
+    app = feathersFakes.app(); // stub Feathers app
+    users = feathersFakes.users(app, db); // mock users service, passing it the test database
+
+    // This is equivalent to app.configure(verifyResetService());
+    verifyResetService().call(app);
+    // Internally verifyResetService() attaches itself as a service with
+    // app.use('/verifyReset/:action/:value', {...});
+    // So now we get a handle to that service
+    verifyReset = app.service('/verifyReset/:action/:value');
+  });
+
+  it('updates unverified user', (done) => {
+    const email = 'a';
+    verifyReset.create({ action: 'resend', value: email }, {}, (err, user) => {
+      assert.strictEqual(err, null, 'err code set');
+
+      // Check user record modified as expected
+      assert.strictEqual(user.isVerified, false, 'isVerified not false');
+      assert.isString(user.verifyToken, 'verifyToken not String');
+      assert.equal(user.verifyToken.length, 30, 'verify token wrong length');
+
+      // Check database record is identical
+      assert.deepEqual(user, db[0]);
+
+      done();
+    });
+  });
+});
 ```
 
 ## Motivation
 
-To do.
+Unit tests are better tests than integration-only tests.
+It can however be difficult to fake a dependencies of a piece of code.
+This package shows an approach to faking the most common dependencies in a Feathers' project.
 
 ## Installation
 
@@ -33,6 +76,7 @@ It will run on Node 6+ without transpiling.
 ## API Reference
 
 See Code Example.
+See `src/index.js` for details.
 
 ## Tests
 
---
+++ test/example.js
@@ -0,0 +1,107 @@
+
+/* global assert, describe, it */
+/* eslint  no-param-reassign: 0, no-shadow: 0, no-unused-vars: 0, no-var: 0, one-var: 0,
+one-var-declaration-per-line: 0 */
+
+const assert = require('chai').assert;
+const feathersFakes = require('../lib');
+const verifyResetService = require('feathers-service-verify-reset').service;
+
+const defaultVerifyDelay = 1000 * 60 * 60 * 24 * 5; // 5 days
+
+// user DB
+
+const now = Date.now();
+const usersDb = [
+  { _id: 'a', email: 'a', isVerified: false, verifyToken: '000', verifyExpires: now + 50000 },
+  { _id: 'b', email: 'b', isVerified: true, verifyToken: null, verifyExpires: null },
+];
+
+/*
+  Test the generic service require('feathers-service-verify-reset').service
+  which works with feathers' standard users service.
+
+  We want to perform unit tests on feathers-service-verify-reset's resend method,
+  so we need to fake its feathers dependencies app and users.
+  We would be running integration tests if we used feathers' actual app and users.
+
+  We will therefore stub out app (which means we cannot test app's internal state),
+  and mock users (which means we can get info about its internal state, e.g. its DB).
+ */
+
+describe('verifyReset::resend', () => {
+  var db;
+  var app;
+  var users;
+  var verifyReset;
+
+  beforeEach(() => {
+    db = clone(usersDb);
+    app = feathersFakes.app(); // stub feathers app
+    users = feathersFakes.users(app, db); // mock users service, passing it its fake database
+
+    // Equivalent to app.configure(verifyResetService());
+    verifyResetService().call(app);
+    // Internally verifyResetService() attaches itself as a service with
+    // app.use('/verifyReset/:action/:value', {...});
+    // So now we get a handle to that service
+    verifyReset = app.service('/verifyReset/:action/:value');
+  });
+
+  it('verifyReset::create exists', () => {
+    assert.isFunction(verifyReset.create);
+  });
+
+  it('updates unverified user', (done) => {
+    const email = 'a';
+    verifyReset.create({ action: 'resend', value: email }, {}, (err, user) => {
+      assert.strictEqual(err, null, 'err code set');
+
+      // Check user record modified as expected
+      assert.strictEqual(user.isVerified, false, 'isVerified not false');
+      assert.isString(user.verifyToken, 'verifyToken not String');
+      assert.equal(user.verifyToken.length, 30, 'verify token wrong length');
+      aboutEqualDateTime(user.verifyExpires, makeDateTime());
+
+      // Check database record is identical
+      assert.deepEqual(user, db[0]);
+
+      done();
+    });
+  });
+
+  it('error on verified user', (done) => {
+    const email = 'b';
+    verifyReset.create({ action: 'resend', value: email }, {}, (err, user) => {
+      assert.equal(err.message, 'User is already verified.');
+
+      done();
+    });
+  });
+
+  it('error on email not found', (done) => {
+    const email = 'x';
+    verifyReset.create({ action: 'resend', value: email }, {}, (err, user) => {
+      assert.equal(err.message, `Email "${email}" not found.`);
+
+      done();
+    });
+  });
+});
+
+// Helpers
+
+function makeDateTime(options1) {
+  options1 = options1 || {};
+  return Date.now() + (options1.delay || defaultVerifyDelay);
+}
+
+function aboutEqualDateTime(time1, time2, msg, delta) {
+  delta = delta || 500;
+  const diff = Math.abs(time1 - time2);
+  assert.isAtMost(diff, delta, msg || `times differ by ${diff}ms`);
+}
+
+function clone(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
